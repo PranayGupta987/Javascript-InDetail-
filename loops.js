a238cf64aeae4bea2f8a1cfbdd46b0b54c1a23c9
PranayGupta987 <pg30122005@gmail.com>
2025-11-09 01:52:53 +0530

    loops in js

---
+++ loops.js
@@ -0,0 +1,24 @@
+//for of loop
+
+
+// in the below example s is the index of the string
+
+// let str = "Hello Pranay";
+
+// for (s in str) {
+//     console.log(str[s]);
+// }
+
+
+
+// for in loop used in objects
+
+
+// let obj = {
+//     name: "Pranay",
+//     number: 102309025
+// }
+
+// for (key in obj) {
+//     console.log(obj[key]);
+// }
