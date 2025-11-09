49c86a17f91474a8075bceb83e6fe2a07d9d2096
PranayGupta987 <pg30122005@gmail.com>
2025-11-09 11:41:13 +0530

    easy shit bro

---
+++ functions&Methods.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    console.log("My name is Pranay");
+    console.log("Hi");
+}
+
+//myFunction();
+
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+//console.log(sum(1,2));
+
+
+
+
+//callbacks in javascript
+
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// arr.forEach((val) => {
+//     console.log(val);
+// })
+
+
+
+//Callbacks in javascript are functions passed to another function
+
+
+
